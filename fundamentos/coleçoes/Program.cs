199146dc9c4d6cbd9ad3a0f510b3f0fc34886904
Jhonathan Garzaro Lopes <104940362+Jhonathan-Garzaro@users.noreply.github.com>
2022-06-14 16:47:41 -0300

    Add files via upload

---
+++ fundamentos/coleçoes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace coleçoes
+{
+      class Program
+    {
+        static void Main(string[] args)
+        { 
+            // Array Multidimensional 
+            int [,] matriz = new int [4, 2]
+            {       {20, 30},
+                    {60, 90},
+                    {40, 80},
+                    {50, 100}
+
+            };
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                
+            }
+            }
+
+
+            
+        }
+    }
+}
